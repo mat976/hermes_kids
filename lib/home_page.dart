bb17dd02c7a14746a80798f15867315b501a8c84
Demontis matisse <45233433+mat976@users.noreply.github.com>
2023-06-01 17:09:38 +0200

    semi fix

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -26,54 +26,101 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: null,
-      body: Center(
-        child: _pages[_selectedIndex],
+      body: IndexedStack(
+        index: _selectedIndex,
+        children: _pages,
       ),
-      bottomNavigationBar: Container(
-        decoration: BoxDecoration(
-          color: Colors.white,
-          boxShadow: [
-            BoxShadow(
-              blurRadius: 20,
-              color: Colors.black.withOpacity(.1),
-            )
-          ],
-        ),
-        child: SafeArea(
-          child: Padding(
-            padding:
-                const EdgeInsets.symmetric(horizontal: 15.0, vertical: 8.0),
-            child: GNav(
-              selectedIndex: _selectedIndex,
-              onTabChange: (index) {
-                setState(() {
-                  _selectedIndex = index;
-                });
-              },
-              tabs: const [
-                GButton(
-                  icon: Icons.home,
-                  text: 'Accueil',
-                  gap: 8, // Augmente l'espace entre l'icône et le texte
+      bottomNavigationBar: LayoutBuilder(
+        builder: (context, constraints) {
+          final width = constraints.maxWidth;
+          final bool isSmallScreen = width < 360;
+          final bool showText = width >= 480;
+          final double fontSize = isSmallScreen ? 12.0 : 14.0;
+
+          return Material(
+            elevation: isSmallScreen ? 4.0 : 0.0,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+              ),
+              child: SafeArea(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    _buildNavItem(
+                      Icons.home,
+                      'Accueil',
+                      isSmallScreen,
+                      showText,
+                      fontSize,
+                      0,
+                    ),
+                    _buildNavItem(
+                      Icons.search,
+                      'Recherche',
+                      isSmallScreen,
+                      showText,
+                      fontSize,
+                      1,
+                    ),
+                    _buildNavItem(
+                      Icons.favorite,
+                      'Favoris',
+                      isSmallScreen,
+                      showText,
+                      fontSize,
+                      2,
+                    ),
+                    _buildNavItem(
+                      Icons.settings,
+                      'Paramètres',
+                      isSmallScreen,
+                      showText,
+                      fontSize,
+                      3,
+                    ),
+                  ],
                 ),
-                GButton(
-                  icon: Icons.search,
-                  text: 'Recherche',
-                  gap: 8, // Augmente l'espace entre l'icône et le texte
-                ),
-                GButton(
-                  icon: Icons.favorite,
-                  text: 'Favoris',
-                  gap: 8, // Augmente l'espace entre l'icône et le texte
-                ),
-                GButton(
-                  icon: Icons.settings,
-                  text: 'Paramètres',
-                  gap: 8, // Augmente l'espace entre l'icône et le texte
-                ),
-              ],
+              ),
             ),
-          ),
+          );
+        },
+      ),
+    );
+  }
+
+  Widget _buildNavItem(
+    IconData icon,
+    String label,
+    bool isSmallScreen,
+    bool showText,
+    double fontSize,
+    int index,
+  ) {
+    return InkWell(
+      onTap: () {
+        setState(() {
+          _selectedIndex = index;
+        });
+      },
+      child: Container(
+        padding: const EdgeInsets.symmetric(vertical: 8.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Icon(
+              icon,
+              size: isSmallScreen ? 24.0 : 28.0,
+            ),
+            if (showText && !isSmallScreen)
+              FittedBox(
+                fit: BoxFit.scaleDown,
+                child: Text(
+                  label,
+                  style: TextStyle(fontSize: fontSize),
+                ),
+              ),
+          ],
         ),
       ),
     );
