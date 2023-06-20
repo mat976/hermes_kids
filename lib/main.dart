3faacc29bf480c0299bdedce2465f878035fcaa2
Demontis matisse <45233433+mat976@users.noreply.github.com>
2023-06-20 16:00:09 +0200

    petites modif

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_config.dart';
+import 'firebase_options.dart';
 import 'login_page.dart';
 import 'register_page.dart';
 import 'home_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await FirebaseConfig.initialize();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
